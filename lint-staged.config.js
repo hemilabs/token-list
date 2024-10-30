2fb6491a3fa3752165150f6de4ec866f1cd2e3b7
Gonzalo D'Elia <gonzalo@bloq.com>
2024-10-30 18:00:42 -0300

    Fix warning in lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,6 @@ const sortPackageJson = "better-sort-package-json";
 module.exports = {
   "!(*.{js,json,md,ts,tsx,yml}|package.json)": [formatFiles],
   "*.{js,md,ts,tsx,yml}": [formatFiles],
-  "src/*.{json}": [formatFiles],
+  "src/*.json": [formatFiles],
   "package.json": [sortPackageJson, formatFiles],
 };
