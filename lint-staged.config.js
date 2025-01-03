e5cfe48b7324cf8e1a8dc962292855e9f469e930
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-01-03 12:13:39 -0300

    Add eslint, simplify scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,9 @@
+const eslint = "eslint --cache --fix --quiet";
 const formatFiles = "prettier --ignore-unknown --write";
 const sortPackageJson = "better-sort-package-json";
 
 export default {
+  "!(*.{js,json,md,ts,tsx,yml}|package.json)": [eslint, formatFiles],
   "!package.json": [formatFiles],
   "package.json": [sortPackageJson, formatFiles],
 };
