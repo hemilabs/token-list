1abd84776a71d485913a950b47486ba8ab7b56bc
Gabriel Montes <gabriel@hemi.xyz>
2025-01-27 15:05:41 -0500

    Improve version sync

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -5,11 +5,24 @@ import fs from "node:fs";
 const packageJson = JSON.parse(fs.readFileSync("./package.json"));
 const tokenList = JSON.parse(fs.readFileSync("./src/hemi.tokenlist.json"));
 
-const [major, minor, patch] = packageJson.version.split(".");
-tokenList.version.major = parseInt(major);
-tokenList.version.minor = parseInt(minor);
-tokenList.version.patch = parseInt(patch);
+const currentVersion = [
+  tokenList.version.major,
+  tokenList.version.minor,
+  tokenList.version.patch,
+].join(".");
 
-tokenList.timestamp = new Date().toISOString();
+if (currentVersion !== packageJson.version) {
+  const [major, minor, patch] = packageJson.version.split(".");
+  tokenList.version.major = parseInt(major);
+  tokenList.version.minor = parseInt(minor);
+  tokenList.version.patch = parseInt(patch);
 
-fs.writeFileSync("src/hemi.tokenlist.json", JSON.stringify(tokenList, null, 2));
+  tokenList.timestamp = new Date().toISOString();
+
+  fs.writeFileSync(
+    "src/hemi.tokenlist.json",
+    JSON.stringify(tokenList, null, 2),
+  );
+
+  console.log("Version updated");
+}
