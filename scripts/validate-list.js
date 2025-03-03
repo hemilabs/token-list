7ce01f73bc651f10f4eef43752fcd563479e99da
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-03-03 16:51:59 -0300

    Add  Remote Bridged mBTC token

--- scripts/validate-list.js
+++ scripts/validate-list.js
@@ -10,13 +10,23 @@ const tokenList = JSON.parse(
 const schemaUrl =
   "https://raw.githubusercontent.com/Uniswap/token-lists/main/src/tokenlist.schema.json";
 
+// Some tokens are invalid (they were deployed with a schema that doesn't match the expected Uniswap list).
+// So we must exclude these.
+// TODO see https://github.com/hemilabs/token-list/issues/41
+const tokenAddressToExclude = ["0x0Af3EC6F9592C193196bEf220BC0Ce4D9311527D"];
+
 // See https://github.com/uniswap/token-lists?tab=readme-ov-file#validating-token-lists
 async function validate() {
   const ajv = new Ajv({ allErrors: true, verbose: true });
   addFormats(ajv);
   const schema = await fetch(schemaUrl).then((r) => r.json());
   const validator = ajv.compile(schema);
-  const valid = validator(tokenList);
+  const valid = validator({
+    ...tokenList,
+    tokens: tokenList.tokens.filter(
+      (token) => !tokenAddressToExclude.includes(token.address),
+    ),
+  });
   if (valid) {
     return;
   }
