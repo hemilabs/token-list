e3d199acb4549147ea00678460b8d3fe71079c3b
Thomas Alvarenga <thomas@hemi.xyz>
2025-03-28 15:14:46 -0300

    Escaped backslash on image URL tests

--- test/hemi.tokenlist.test.js
+++ test/hemi.tokenlist.test.js
@@ -91,8 +91,7 @@ describe("List of tokens", function () {
           .toLowerCase();
 
         const filepath = logoURI.match(
-          // eslint-disable-next-line no-useless-escape
-          new RegExp(`^${repoUrl}/master/src/logos/${filename}\.(svg|png)$`),
+          new RegExp(`^${repoUrl}/master/src/logos/${filename}\\.(svg|png)$`),
         );
         assert.notEqual(filepath, null);
         fs.accessSync(filepath[0].replace(`${repoUrl}/master/`, ""));
