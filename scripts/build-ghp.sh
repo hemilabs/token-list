7ff28f7b6ed36735a5e839b7684148bbf39968b6
Gabriel Montes <gabriel@hemi.xyz>
2026-03-31 16:41:00 -0400

    Create hemi.json

--- scripts/build-ghp.sh
+++ scripts/build-ghp.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+# Cleanup build folder
 rm -rf public
-
 mkdir public
+
+# Copy base assets
 cp -r src/hemi.tokenlist.json src/logos src/l1Logos scripts/assets public
 
+# Build index.html
 node scripts/build-ghp-index.js
+
+# Extract hemi.json
+jq '.tokens[] | select(.symbol == "HEMI" and .chainId == 43111)' src/hemi.tokenlist.json >public/hemi.json
